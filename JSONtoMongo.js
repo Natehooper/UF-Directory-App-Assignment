a08820bd437057a59dd6e2980ebb5fd6be7ea05a
Natehooper <natehoop@gmail.com>
2017-09-06 21:06:43 -0400

    changes

--- JSONtoMongo.js
+++ JSONtoMongo.js
@@ -1,23 +1,47 @@
 'use strict';
-/* 
-  Import modules/files you may need to correctly run the script. 
+/*
+  Import modules/files you may need to correctly run the script.
   Make sure to save your DB's uri in the config file, then import it with a require statement!
  */
 var fs = require('fs'),
-    mongoose = require('mongoose'), 
-    Schema = mongoose.Schema, 
-    Listing = require('./ListingSchema.js'), 
+    mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    Listing = require('./ListingSchema.js'),
     config = require('./config');
+  var listings = require('./listings.json');
+  var http = require('http');
+
+  mongoose.connect(config.db.uri);
+
+  var requestHandler = function(request, response) {
+   response.end('Bad gateway error');
+
+   for (var i = 0; i < listings.entries.length; i++) {
+      var newListing = new listing({
+        code: listings.entries[i].code,
+        name: listings.entries[i].name,
+        coordinates: listings.entries[i].coordinates,
+        address: listings.entries[i].address
+      });
+      newListing.save(function(err){
+        if(err) throw err;
+      });
+    }
+    console.log('New user created.');
+};
+
+  http.createServer(requestHandler).listen(5000, function(){ console.log('Test')} );
+
 
 /* Connect to your database */
 
-/* 
-  Instantiate a mongoose model for each listing object in the JSON file, 
-  and then save it to your Mongo database 
+/*
+  Instantiate a mongoose model for each listing object in the JSON file,
+  and then save it to your Mongo database
  */
 
 
-/* 
-  Once you've written + run the script, check out your MongoLab database to ensure that 
-  it saved everything correctly. 
+/*
+  Once you've written + run the script, check out your MongoLab database to ensure that
+  it saved everything correctly.
  */
--- queries.js
+++ queries.js
@@ -1,28 +1,38 @@
 /* Fill out these functions using Mongoose queries*/
 
+var mongoose = require('mongoose');
+var Listing = require('./ListingSchema.js');
+var config = require('./config');
+mongoose.connect(config.db.uri);
+
 var findLibraryWest = function() {
-  /* 
-    Find the document that contains data corresponding to Library West,
-    then log it to the console. 
-   */
+  Listing.findOne({code: 'LBW'},function(err,listing){
+    if(err) throw err;
+    console.log(listing);
+    console.log('\n');
+   });
 };
 var removeCable = function() {
-  /*
-    Find the document with the code 'CABL'. This cooresponds with courses that can only be viewed 
-    on cable TV. Since we live in the 21st century and most courses are now web based, go ahead
-    and remove this listing from your database and log the document to the console. 
-   */
+  Listing.findOneAndRemove({code: 'CABL'}, function(err,listing){
+      if (err) throw err;
+      console.log('CABL Removed.\n' + listing);
+      console.log('\n');
+   })
 };
 var updatePhelpsLab = function() {
-  /*
-    Phelps Laboratory's address is incorrect. Find the listing, update it, and then 
-    log the updated document to the console. 
-   */
+  Listing.findOneAndUpdate({name: 'Phelps Laboratory'}, {$set : {address:'Museum Rd, Gainesville FL, 32603'} } ,{new: true}, function(err, listing){
+
+    if(err) throw err;
+    console.log('Phelps Lab Updated\n');
+    console.log('Listing has been changed to:\n' + listing);
+
+   });
 };
 var retrieveAllListings = function() {
-  /* 
-    Retrieve all listings in the database, and log them to the console. 
-   */
+  Listing.find({},function(err, listing){
+    if(err) throw err;
+    console.log(prompt + listing);
+  });
 };
 
 findLibraryWest();
